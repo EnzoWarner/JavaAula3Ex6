cd210131991cc71049e12d257eba6f5581b197de
EnzoWarner <enzo_warner@hotmail.com>
2024-05-14 02:28:43 -0300

    first commit

---
+++ Java Aula 3 Ex 6.java
@@ -0,0 +1,8 @@
+public class EX3_3 {
+    public static void main (String[] args) {
+    int alt = 5;
+    int larg = 10;
+    int prof = 15;
+    System.out.println("o volume é = "+(alt*larg*prof));
+}
+}
